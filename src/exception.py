bcb2d62f6154db2f7b55faf3f53f2c4bc48dfe03
pavithra282001 <pavithra123aju@gmail.com>
2025-05-04 12:55:11 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() 
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -19,3 +20,8 @@ class customException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+   
+
+
